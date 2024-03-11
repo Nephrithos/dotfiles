741eab4bd777e87cc3d3a594f0d0c73b01dcd41a
Nephrihtos <jarrodasmith@protonmail.com>
2024-03-11 18:56:41 +1000

    modified:   .config/waybar/scripts/update.sh

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -14,7 +14,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qu --aur --quiet | wc -l); then
+if ! updates_aur=$(paru -Qua --quiet | wc -l); then
     updates_aur=0
 fi
 
