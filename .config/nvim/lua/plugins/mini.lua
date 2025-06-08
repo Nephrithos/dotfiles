5a03690c316e9ecfeb14da53d9b4d7b4bd83feed
Nephrithos <jarrodasmith@protonmail.com>
2025-06-08 22:38:29 +1000

    updates to new nvim

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,20 @@
 return {
-	--{ 'echasnovski/mini.nvim', version = false },
-	'echasnovski/mini.statusline', 
-	version = false,
-	opts = {},
+	{
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function()
+			require("mini.pairs").setup()
+			require("mini.surround").setup()
+			require("mini.comment").setup()
+			require("mini.ai").setup()
+			require("mini.icons").setup()
+			--require("mini.extra").setup()
+			require("mini.hipatterns").setup()
+			require("mini.cursorword").setup()
+			require("mini.statusline").setup()
+			--require("mini.animate").setup()
+			require("mini.map").setup()
+			--require("mini.notify").setup()
+		end,
+	},
 }
